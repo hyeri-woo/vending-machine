67107d0346ef9ce825fa55aabd57afadb2be50e8
hyeri-woo <woohyeri0525@gmail.com>
2023-04-29 22:25:01 +0900

    Fix drinkGenerator

--- js/classes/drinkGenerator.js
+++ js/classes/drinkGenerator.js
@@ -10,7 +10,7 @@ class DrinkGenerator {
 
     async loadData() {
         try {
-            const response = await fetch('../items.json');
+            const response = await fetch('./items.json');
             if(response.ok) {   // 서버의 응답 코드가 200 ~ 299일 경우
                 return response.json();
             } else {
